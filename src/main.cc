e82a45a79fea8881a699fb56017771908533b6c5
R4YL0 <Khaled.kottmann@gmail.com>
2025-01-12 11:09:23 +0100

    Updated main

--- src/main.cc
+++ src/main.cc
@@ -6,9 +6,13 @@
 #include "scenes.h"
 
 #include <chrono>
+#include <windows.h>
 using namespace std::chrono;
 
 int main() {
+    //Prevent Sleep
+    SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+
     //World
     hittable_list world;
     hittable_list lights;
@@ -17,15 +21,18 @@ int main() {
     camera cam;
 
     //Scene
-    scene12(world, lights, cam, 400, 20, 20);
+    scene12(world, lights, cam, 1600, 200, 200);
 
     //Renderer w/ Performance measurement
     auto begin = high_resolution_clock::now();
-    cam.render(world, lights, 8, true);
+    cam.render(world, lights, 8, false);
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<seconds>(end - begin);
 
     std::clog << "Render Time: " << duration.count() << " seconds.\n";
+
+    //Allow Sleep
+    SetThreadExecutionState(ES_CONTINUOUS);
 
     return 0;
 }
